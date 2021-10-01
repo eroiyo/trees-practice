4cb92eea6610c4594a5b7d37e2b824bd32e7f3e0
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-10-01 03:24:40 -0400

    tree practice

---
+++ challenge2.js
@@ -0,0 +1,50 @@
+class Node {
+    constructor(data) {
+      this.data = data
+      this.left = null
+      this.right = null
+    }
+  }
+  
+  function arrayToTree(array, i) {
+    if (i >= array.length || array[i] === 0) {
+      return null
+    }
+  
+    const node = new Node(array[i])
+    node.left = arrayToTree(array, 2*i+1)
+    node.right = arrayToTree(array, 2*i+2)
+  
+    return node
+  }
+  
+  function preOrder(node) {
+    if (node === null) {
+      return ''
+    }
+  
+    let result = `${node.data} `
+    result += preOrder(node.left)
+    result += preOrder(node.right)
+    return result
+  }
+  
+  function postOrder(node) {
+    let result = "";
+    if(node.left!=null){
+      result = result  + postOrder(node.left);
+    }
+    if(node.right!=null){
+      result = result + postOrder(node.right);
+    }
+    if(node.data !== undefined){
+      result = result + (node.data) + " ";
+    }
+    return result
+  }
+  
+  const tree = arrayToTree([10, 1, 2, 3, 4, 5, 6], 0)
+  console.log(postOrder(tree))
+  //=> 3 4 1 5 6 2 10
+  module.exports = { arrayToTree, postOrder }
+  
